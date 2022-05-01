0833893988e873b05c124f6f99bbc8b1bb68813f
probonopd <probonopd@users.noreply.github.com>
2022-05-01 16:08:51 +0000

    Remove fuse3-static for now

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ apk update
 apk add alpine-sdk util-linux strace file zlib-dev zlib-static autoconf automake libtool
 
 # Build static squashfuse
-apk add glib-static glib-dev fuse-static fuse3-static zstd-static
+apk add glib-static glib-dev fuse-static zstd-static # fuse3-static
 wget -c -q "https://github.com/vasi/squashfuse/releases/download/0.1.104/squashfuse-0.1.104.tar.gz"
 tar xf squashfuse-*.tar.gz
 cd squashfuse-*/
