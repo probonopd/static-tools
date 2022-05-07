35f2fca4ab9b5bb72c20400103201e6a62d0f27d
probonopd <probonopd@users.noreply.github.com>
2022-05-07 13:36:39 +0200

    appimage_hexlify

--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -1310,6 +1310,21 @@ Md5Calculate
 //  End of WjCryptLib_Md5
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+char* appimage_hexlify(const char* bytes, const size_t numBytes) {
+    // first of all, allocate the new string
+    // a hexadecimal representation works like "every byte will be represented by two chars"
+    // additionally, we need to null-terminate the string
+    char* hexlified = (char*) calloc((2 * numBytes + 1), sizeof(char));
+
+    for (size_t i = 0; i < numBytes; i++) {
+        char buffer[3];
+        sprintf(buffer, "%02x", (unsigned char) bytes[i]);
+        strcat(hexlified, buffer);
+    }
+
+    return hexlified;
+}
+
 int main(int argc, char *argv[]) {
     char appimage_path[PATH_MAX];
     char argv0_path[PATH_MAX];
